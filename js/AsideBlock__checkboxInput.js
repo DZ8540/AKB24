d3035eab566f0fd9f2760bb6e260471889cee23d
8DZ8 <d.z.2021@inbox.ru>
2021-03-15 13:28:14 +0300

    Added searchBar for products

--- js/AsideBlock__checkboxInput.js
+++ js/AsideBlock__checkboxInput.js
@@ -1,6 +1,10 @@
+let brands = [];
 let checkboxes = $('.AsideBlock__checkboxLabel');
+
 $.each(checkboxes, function (index, checkbox) {
+  console.log(checkbox);
   $(checkbox).on('click', function() {
     $(this).toggleClass('AsideBlock__checkboxInputActive');
+    brands.push(this.previousElementSibling.id);
   });
 });
